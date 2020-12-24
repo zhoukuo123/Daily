3d569305a1c56079c3b720303af55eadde57f19d
zhoukuo123 <1178053078@qq.com>
2020-12-24 12:59:40 +0800

    update

--- first-listener/src/main/java/com/zk/listener/FirstListener.java
+++ first-listener/src/main/java/com/zk/listener/FirstListener.java
@@ -1,7 +1,18 @@
 package com.zk.listener;
 
+import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class FirstListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("ServletContext已初始化");
+    }
 
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("ServletContext已销毁");
+    }
 }
