f7b17bf53a967cb773472af70f034350e058f503
zhoukuo123 <1178053078@qq.com>
2020-12-24 21:38:02 +0800

    update

--- request-total/src/main/java/com/zk/total/RequestTotalListener.java
+++ request-total/src/main/java/com/zk/total/RequestTotalListener.java
@@ -1,6 +1,7 @@
 package com.zk.total;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -28,10 +29,15 @@ public class RequestTotalListener implements ServletContextListener, ServletRequ
     public void requestInitialized(ServletRequestEvent sre) {
 //        List<String> timeList = (List<String>) sre.getServletContext().getAttribute("timeList");
 //        List<Integer> valueList = (List<Integer>) sre.getServletContext().getAttribute("valueList");
+        HttpServletRequest req = (HttpServletRequest) sre.getServletRequest();
+        String url = req.getRequestURL().toString();
+        if (url.endsWith("/rt")) {
+            return;
+        }
         Map<String, Integer> totalMap = (Map<String, Integer>) sre.getServletContext().getAttribute("totalMap");
-        Date data = new Date();
+        Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
-        String time = simpleDateFormat.format(data);
+        String time = simpleDateFormat.format(date);
         System.out.println(time);
         if (totalMap.containsKey(time)) {
             Integer value = totalMap.get(time);
@@ -45,5 +51,5 @@ public class RequestTotalListener implements ServletContextListener, ServletRequ
     @Override
     public void requestDestroyed(ServletRequestEvent sre) {
 
-}
+    }
 }
--- request-total/src/main/java/com/zk/total/RequestTotalServlet.java
+++ request-total/src/main/java/com/zk/total/RequestTotalServlet.java
@@ -1,5 +1,7 @@
 package com.zk.total;
 
+import com.alibaba.fastjson.JSON;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 @SuppressWarnings("unchecked")
 @WebServlet("/rt")
@@ -20,8 +21,20 @@ public class RequestTotalServlet extends HttpServlet {
         Map<String, Integer> totalMap = (Map<String, Integer>) servletContext.getAttribute("totalMap");
         resp.setContentType("text/html; charset=utf-8");
         PrintWriter out = resp.getWriter();
+//        totalMap.forEach((String time, Integer value) -> {
+//            out.println(time + "->" + value);
+//        });
+        List<String> timeList = new ArrayList<>();
+        List<Integer> valueList = new ArrayList<>();
         totalMap.forEach((String time, Integer value) -> {
-            out.println(time + ":" + value);
+            timeList.add(time);
+            valueList.add(value);
         });
+        Map<String, Object> map = new HashMap<>();
+        map.put("timeList", timeList);
+        map.put("valueList", valueList);
+        String jsonString = JSON.toJSONString(map);
+//        System.out.println(jsonString);
+        out.println(jsonString);
     }
 }
