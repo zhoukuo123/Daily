470840800a436afead40f86d151adab47bf2c7f1
zhoukuo123 <1178053078@qq.com>
2021-02-20 22:57:13 +0800

    update

--- mgallery/src/main/java/com/zk/mgallery/utils/PageModel.java
+++ mgallery/src/main/java/com/zk/mgallery/utils/PageModel.java
@@ -1,4 +1,16 @@
 package com.zk.mgallery.utils;
 
+import java.util.List;
+
+// 分页模型对象
 public class PageModel {
+    private int page; //
+    private int totalPages;
+    private int rows;
+    private int totalRows;
+    private int pageStartRow;
+    private int pageEndRow;
+    private boolean hasNextPage;
+    private boolean hasPreviousPage;
+    private List pageData;
 }
