4ab2045193865100c447c84c90796c3ef1912937
zhoukuo123 <1178053078@qq.com>
2021-02-25 22:07:43 +0800

    update

---
+++ mgallery/src/main/java/com/zk/mgallery/controller/PaintingController.java
@@ -0,0 +1,36 @@
+package com.zk.mgallery.controller;
+
+import com.zk.mgallery.service.PaintingService;
+import com.zk.mgallery.utils.PageModel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/page")
+public class PaintingController extends HttpServlet {
+    private PaintingService paintingService = new PaintingService();
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 1. 接收Http数据
+        String page = request.getParameter("p"); // 页号
+        String rows = request.getParameter("r"); // 每页记录数
+        if (page == null) { // page设置默认值
+            page = "1";
+        }
+        if (rows == null) { // rows设置默认值
+            rows = "6";
+        }
+
+        // 2. 调用Service方法, 得到处理结果
+        PageModel pageModel = paintingService.pagination(Integer.parseInt(page), Integer.parseInt(rows));
+        request.setAttribute("pageModel", pageModel);
+
+        // 3. 请求转发至对应JSP(view)进行数据展现
+        request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+    }
+}
--- mgallery/src/main/java/com/zk/mgallery/service/PaintingService.java
+++ mgallery/src/main/java/com/zk/mgallery/service/PaintingService.java
@@ -7,11 +7,17 @@ import com.zk.mgallery.utils.PageModel;
 import java.util.List;
 
 /**
- *
+ * PaintingService油画服务类
  */
 public class PaintingService {
     private PaintingDao paintingDao = new PaintingDao();
 
+    /**
+     * 数据分页查询
+     * @param page 页号
+     * @param rows 每页记录数
+     * @return 分页对象
+     */
     public PageModel pagination(int page, int rows) {
         if (rows == 0) {
             throw new RuntimeException("无效的rows参数");
@@ -21,7 +27,9 @@ public class PaintingService {
 
     public static void main(String[] args) {
         PaintingService paintingService = new PaintingService();
+
         PageModel pageModel = paintingService.pagination(2, 6);
+
         List<Painting> paintingList = pageModel.getPageData();
         for (Painting painting : paintingList) {
             System.out.println(painting.getPname());
