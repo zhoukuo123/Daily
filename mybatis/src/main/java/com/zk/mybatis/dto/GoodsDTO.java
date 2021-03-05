4ddd920bf4b1f9621be8aa9ce808390a482021f3
zhoukuo123 <1178053078@qq.com>
2021-03-05 22:03:56 +0800

    update

---
+++ mybatis/src/main/java/com/zk/mybatis/dto/GoodsDTO.java
@@ -0,0 +1,37 @@
+package com.zk.mybatis.dto;
+
+import com.zk.mybatis.entity.Category;
+import com.zk.mybatis.entity.Goods;
+
+/**
+ * Data Transfer Object -- 数据传输对象
+ */
+public class GoodsDTO {
+    private Goods goods = new Goods();
+    private Category category = new Category();
+    private String test;
+
+    public Goods getGoods() {
+        return goods;
+    }
+
+    public void setGoods(Goods goods) {
+        this.goods = goods;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
--- mybatis/src/test/java/com/zk/mybatis/MyBatisTestor.java
+++ mybatis/src/test/java/com/zk/mybatis/MyBatisTestor.java
@@ -1,5 +1,6 @@
 package com.zk.mybatis;
 
+import com.zk.mybatis.dto.GoodsDTO;
 import com.zk.mybatis.entity.Goods;
 import com.zk.mybatis.utils.MyBatisUtils;
 import org.apache.ibatis.io.Resources;
@@ -11,7 +12,9 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // JUnit单元测试类
 public class MyBatisTestor {
@@ -65,6 +68,163 @@ public class MyBatisTestor {
                  System.out.println(goods.getTitle());
             }
         } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testSelectById() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Goods goods = sqlSession.selectOne("goods.selectById", 1603);
+            System.out.println(goods.getTitle());
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testSelectByPriceRange() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Map<String, Integer> param = new HashMap<>();
+            param.put("min", 100);
+            param.put("max", 500);
+            param.put("limit", 10);
+            List<Goods> list = sqlSession.selectList("selectByPriceRange", param);
+            for (Goods goods : list) {
+                System.out.println(goods.getTitle() + ":" + goods.getCurrentPrice());
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testSelectGoodsMap() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            List<Map<String, Object>> list = sqlSession.selectList("selectGoodsMap");
+            for (Map<String, Object> map : list) {
+                System.out.println(map);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testSelectGoodsDTO() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            List<GoodsDTO> list = sqlSession.selectList("selectGoodsDTO");
+            for (GoodsDTO goodsDTO : list) {
+                System.out.println(goodsDTO.getGoods().getTitle());
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testInsert() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+
+            Goods goods = new Goods();
+            goods.setTitle("测试商品");
+            goods.setSubTitle("测试子标题");
+            goods.setOriginalCost(200f);
+            goods.setCurrentPrice(100f);
+            goods.setDiscount(0.5f);
+            goods.setIsFreeDelivery(1);
+            goods.setCategoryId(43);
+
+            // insert()方法返回值代表本次成功插入的记录总数
+            int num = sqlSession.insert("insert", goods);
+            sqlSession.commit(); // 提交事务数据
+            System.out.println(goods.getGoodsId());
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback(); // 回滚事务
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Goods goods = sqlSession.selectOne("goods.selectById", 739);
+
+            goods.setTitle("更新测试商品");
+
+            int num = sqlSession.update("update", goods);
+            sqlSession.commit(); // 提交事务数据
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback(); // 回滚事务
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testDelete() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+
+            int num = sqlSession.delete("delete", 739);
+            sqlSession.commit(); // 提交事务数据
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback(); // 回滚事务
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testSelectByTitle() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Map<String, String> param = new HashMap<>();
+            param.put("title", "'' or 1 = 1");
+            param.put("order", " order by title desc");
+
+            List<Goods> list = sqlSession.selectList("selectByTitle", param);
+            for (Goods goods : list) {
+                System.out.println(goods.getTitle() + ":" + goods.getCurrentPrice());
+            }
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback(); // 回滚事务
+            }
             throw e;
         } finally {
             MyBatisUtils.closeSession(sqlSession);
