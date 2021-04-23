c84b1fa80acede991212675f52e249d60e2c4113
zhoukuo123 <1178053078@qq.com>
2021-04-23 17:18:06 +0800

    update

--- spring03-jdbc/src/main/java/com/zk/spring/jdbc/service/EmployeeService.java
+++ spring03-jdbc/src/main/java/com/zk/spring/jdbc/service/EmployeeService.java
@@ -4,6 +4,8 @@ import com.zk.spring.jdbc.dao.EmployeeDao;
 import com.zk.spring.jdbc.entity.Employee;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.Date;
@@ -12,6 +14,7 @@ import java.util.Date;
  * @author CoderZk
  */
 @Service
+@Transactional
 public class EmployeeService {
     @Resource
     private EmployeeDao employeeDao;
@@ -21,6 +24,11 @@ public class EmployeeService {
 
     @Resource
     private DataSourceTransactionManager transactionManager;
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED, readOnly = true)
+    public Employee findById(Integer eno) {
+        return employeeDao.findById(eno);
+    }
 
     public void batchImport() {
         for (int i = 1; i <= 10; i++) {
