5963d6b95c5dd3665b07a76aefecd0c3f4a9a78c
zhoukuo123 <1178053078@qq.com>
2021-04-26 16:53:09 +0800

    update

--- restful/src/main/java/com/zk/restful/controller/RestfulController.java
+++ restful/src/main/java/com/zk/restful/controller/RestfulController.java
@@ -1,19 +1,70 @@
 package com.zk.restful.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.zk.restful.entity.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author CoderZk
  */
-@Controller
+@RestController
 @RequestMapping("/restful")
+@CrossOrigin(origins = {"http://localhost:8080"})
 public class RestfulController {
     @GetMapping("/request")
-    @ResponseBody
     public String doGetRequest() {
         return "{\"message\":\"返回查询结果\"}";
+    }
+
+    // POST /article/1
+    // POST /restful/request/100
+    @PostMapping("/request/{rid}")
+    public String doPostRequest(@PathVariable("rid") Integer requestId, Person person) {
+        System.out.println(person.getName() + ":" + person.getAge());
+        return "{\"message\":\"数据新建成功\", \"id\":" + requestId + "}";
+    }
+
+    @PutMapping("/request")
+    public String doPutRequest(Person person) {
+        System.out.println(person.getName() + ":" + person.getAge());
+        return "{\"message\":\"数据更新成功\"}";
+    }
+
+    @DeleteMapping("/request")
+    public String doDeleteRequest() {
+        return "{\"message\":\"数据删除成功\"}";
+    }
+
+    @GetMapping("/person")
+    public Person findByPersonId(Integer id) {
+        Person person = new Person();
+        if (id == 1) {
+            person.setName("lily");
+            person.setAge(23);
+        } else if (id == 2) {
+            person.setName("smith");
+            person.setAge(22);
+        }
+        return person;
+    }
+
+    @GetMapping("persons")
+    public List<Person> findPersons() {
+        List<Person> list = new ArrayList<>();
+        Person p1 = new Person();
+        p1.setName("lily");
+        p1.setAge(23);
+        p1.setBirthday(new Date());
+
+        Person p2 = new Person();
+        p2.setName("smith");
+        p2.setAge(22);
+        p2.setBirthday(new Date());
+        list.add(p1);
+        list.add(p2);
+        return list;
     }
 }
