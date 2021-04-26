1d99ea6304a7b345b69f6e54e64a045c7cb2c88d
zhoukuo123 <1178053078@qq.com>
2021-04-26 22:23:01 +0800

    update

--- spring-boot-learn/src/main/java/com/zk/springbootlearn/PropertiesController.java
+++ spring-boot-learn/src/main/java/com/zk/springbootlearn/PropertiesController.java
@@ -1,5 +1,7 @@
 package com.zk.springbootlearn;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PropertiesController {
+    @Value("${school.grade}")
     Integer grade;
+
+    @Value("${school.classnum}")
     Integer classnum;
 
+    @GetMapping("/gradeclass")
     public String gradeClass() {
         return "年级: " + grade + " 班级: " + classnum;
     }
---
+++ spring-boot-learn/src/main/java/com/zk/springbootlearn/SchoolConfig.java
@@ -0,0 +1,32 @@
+package com.zk.springbootlearn;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author CoderZk
+ * School配置类
+ */
+@Component
+@ConfigurationProperties(prefix = "school")
+public class SchoolConfig {
+    Integer grade;
+
+    Integer classnum;
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public Integer getClassnum() {
+        return classnum;
+    }
+
+    public void setClassnum(Integer classnum) {
+        this.classnum = classnum;
+    }
+}
