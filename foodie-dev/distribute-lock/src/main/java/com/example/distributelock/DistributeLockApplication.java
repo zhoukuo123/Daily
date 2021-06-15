c7a2a51174926d65998771472875819cd4cd1742
zhoukuo123 <1178053078@qq.com>
2021-06-15 18:14:50 +0800

    update distribute lock demo (db, redis)

---
+++ foodie-dev/distribute-lock/src/main/java/com/example/distributelock/DistributeLockApplication.java
@@ -0,0 +1,20 @@
+package com.example.distributelock;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author CoderZk
+ */
+@SpringBootApplication
+@MapperScan("com.example.distributelock.dao")
+@EnableScheduling
+public class DistributeLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeLockApplication.class, args);
+    }
+
+}
