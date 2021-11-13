429947f56023ebfb9aa52ce8900f4a84d4edf126
zhoukuo123 <1178053078@qq.com>
2021-11-13 19:07:27 +0800

    update

---
+++ foodie-cloud/platform/hystrix-turbine/src/main/java/com/zk/HystrixTurbineApplication.java
@@ -0,0 +1,22 @@
+package com.zk;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author CoderZk
+ */
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableAutoConfiguration
+public class HystrixTurbineApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixTurbineApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
