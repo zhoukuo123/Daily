413174b7f8ce1298ee38c80dee6f5c977b2049a1
zhoukuo123 <1178053078@qq.com>
2021-11-15 16:19:47 +0800

    update

--- foodie-cloud/domain/user/foodie-user-web/src/main/java/com/zk/user/UserApplication.java
+++ foodie-cloud/domain/user/foodie-user-web/src/main/java/com/zk/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.zk.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -15,6 +16,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.zk", "org.n3r.idworker"})
 @EnableDiscoveryClient
+@EnableHystrix
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
