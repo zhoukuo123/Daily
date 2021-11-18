2f644c33cad0fc46f926e0770a9a921aee031302
zhoukuo123 <1178053078@qq.com>
2021-11-18 20:10:28 +0800

    update

--- spring-cloud-demo/config/config-client/src/main/java/com/zk/springcloud/ConfigClientApplication.java
+++ spring-cloud-demo/config/config-client/src/main/java/com/zk/springcloud/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.zk.springcloud;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author CoderZk
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConfigClientApplication.class)
